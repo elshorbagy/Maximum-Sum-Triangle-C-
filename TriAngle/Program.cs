dd40d7608813a77d1f608fe0fb372133df375562
elshorbagy76 <elshorbagy76@hotmail.com>
2019-11-20 19:00:28 +0200

    Add project files.

---
+++ TriAngle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TriAngle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
